cac0cc23f360c56e9fed35eb475848b412967006
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-15 20:43:21 -0300

    feat : H2 database test profile

--- course/src/main/java/com/example/course/resources/UsersResource.java
+++ course/src/main/java/com/example/course/resources/UsersResource.java
@@ -11,12 +11,9 @@ import com.example.course.entities.User;
 @RequestMapping(value = "/users") //deve-se dar um nome para o recurso(value = "caminho do recurso)
 public class UsersResource {
 	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
+	@GetMapping //indica que o método responde a uma requisição GET
+	public ResponseEntity<User> findAll(){ // esse método é um endpoint que acessa usuarios
 		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "123456");
 		return ResponseEntity.ok().body(u);
 	};
-	
-	
-
 }
