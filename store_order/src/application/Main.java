765be5f2c5e13b063fa0e0fb16f76fcab584b4f7
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-23 18:34:11 -0300

    feat: primeiras frases

---
+++ store_order/src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        System.out.print("Email: ");
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        System.out.println("Enter order data:");
+        System.out.print("Name: ");
+        System.out.print("Status: ");
+        System.out.print("How many items to this order? ");
+    }
+}
